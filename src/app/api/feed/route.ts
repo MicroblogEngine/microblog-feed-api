68a705d2fab8669b9e6b7a601748b71095edd3a3
Rogerio Pereira Araujo <rogerio.araujo@gmail.com>
2024-12-16 16:13:48 -0300

    Small refactoring.

--- src/app/api/feed/route.ts
+++ src/app/api/feed/route.ts
@@ -1,9 +1,9 @@
-import { fakeUsers } from "@/db/users";
+import { fakeFeed } from "@/db/feed";
 import { NextResponse } from "next/server";
 
 export async function GET() {
 
-    return new NextResponse(JSON.stringify(fakeUsers), {
+    return new NextResponse(JSON.stringify(fakeFeed), {
         status: 200,
     });
 }
---
+++ src/app/db/feed.ts
@@ -0,0 +1,19 @@
+interface Post {
+  id: number,
+  text: string,
+}
+
+export const fakeFeed: Post[] = [
+  {
+    id: 1,
+    text: "Sarthak Roy",
+  },
+  {
+    id: 2,
+    text: "John McNormie",
+  },
+  {
+    id: 3,
+    text: "John Doe",
+  },
+];
