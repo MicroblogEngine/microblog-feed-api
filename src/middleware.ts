8524f420751ca7d36b8115d1ff9b8c8caad13b39
Rogerio Pereira Araujo <rogerio.araujo@gmail.com>
2025-01-18 09:12:04 -0300

    Enabling telemetry.

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,32 +6,8 @@ const ignoredRoutes = [
   '/api/feed/metrics',
 ]
 
-const allowedOrigins = [
-  'https://appmicroblogararog.loclx.io', 
-  'https://app.microblog.training', 
-  'http://microblog-prometheus.default.svc.cluster.local', 
-];
-
 export async function middleware(req:NextRequest) {
   const res = NextResponse.next();
-
-  const origin = req.headers.get("origin");
-  if (! origin)
-    return res;
-
-  // if the origin is an allowed one,
-  // add it to the 'Access-Control-Allow-Origin' header
-  if (allowedOrigins.includes(origin)) {
-    res.headers.append('Access-Control-Allow-Origin', origin);
-  }
-
-  // add the remaining CORS headers to the response
-  res.headers.append('Access-Control-Allow-Credentials', "true")
-  res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
-  res.headers.append(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
 
   if(! ignoredRoutes.includes(req.url)) {
     const authorization = req.headers.get('authorization');
