f39a3f23b04f89a2f665dfa1aec083ad0aae3fc9
Rogerio Pereira Araujo <rogerio.araujo@gmail.com>
2025-02-03 10:19:39 -0300

    [chore] updating depedencies and references

--- apps/worker-grpc/src/index.ts
+++ apps/worker-grpc/src/index.ts
@@ -1,51 +1,19 @@
 import {createServer, CallContext} from 'nice-grpc';
-import { prisma } from "@ararog/microblog-feed-api-db";
-import {DeepPartial, 
-  CreateProfileRequest, 
-  CreateProfileResponse, 
-  GetProfileByUserIdRequest,
-  GetProfileByUserIdResponse,
-  ProfileServiceDefinition, 
-  ProfileServiceImplementation 
+import {
+  DeepPartial, 
+  PingRequest, 
+  PingResponse, 
+  FeedServiceDefinition, 
+  FeedServiceImplementation 
 } from '@ararog/microblog-rpc';
 
-const profileServiceImpl: ProfileServiceImplementation = {
-  async createProfile(
-    request: CreateProfileRequest,
+const feedServiceImpl: FeedServiceImplementation = {
+  async ping(
+    request: PingRequest,
     context: CallContext
-  ): Promise<DeepPartial<CreateProfileResponse>> {
-    
-    if (!request.userId) {
-      throw new Error('User ID is required');
-    }
-    const profile = await prisma.profile.create({
-      data: {
-        userId: request.userId,
-        name: request.name,
-        birthDate: new Date(request.dateOfBirth)
-      }
-    });
+  ): Promise<DeepPartial<PingResponse>> {
     return {
-      id: profile.id
-    };
-  },
-  async getProfileByUserId(
-    request: GetProfileByUserIdRequest,
-    context: CallContext
-  ): Promise<DeepPartial<GetProfileByUserIdResponse>> {
-    const userId = context.metadata.get('x-user-id');
-    if (!userId) {
-      throw new Error('User ID is required');
-    }
-    const profile = await prisma.profile.findUnique({
-      where: {
-        userId: userId
-      }
-    });
-    // ... method logic
-    return {
-      id: profile?.id,
-      name: profile?.name
+      message: 'Pong'
     };
   },
 };
@@ -54,7 +22,7 @@ const startServer = async () => {
   console.log('Starting server');
   const server = createServer();
 
-  server.add(ProfileServiceDefinition, profileServiceImpl);
+  server.add(FeedServiceDefinition, feedServiceImpl);
 
   console.log('Server listening on', process.env.GRPC_HOST || '0.0.0.0:8080');
   await server.listen(process.env.GRPC_HOST || '0.0.0.0:8080');
