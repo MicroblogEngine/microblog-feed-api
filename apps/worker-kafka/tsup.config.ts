652b7da72c31ca9c7ae916e1dfc7ea7a80e44394
Rogerio Pereira Araujo <rogerio.araujo@gmail.com>
2025-02-19 19:28:45 -0300

    [fix] pairing up code with user-api

--- apps/worker-kafka/tsup.config.ts
+++ apps/worker-kafka/tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   entry: ['./src/index.ts'],
   //https://github.com/egoist/tsup/issues/619
   noExternal: [ "kafkajs", "handlebars", "@sendgrid", "@ararog", "dotenv" ],
-  splitting: false,
+  splitting: true,
   bundle: true,
   target: 'node23',  
   outDir: './dist',
